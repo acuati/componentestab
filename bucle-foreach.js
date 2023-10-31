95a099caa687659fd6408bcf7b54becb97b40cbe
acuati <benalibenaliabdelkbir@gmail.com>
2023-10-31 13:17:11 +0100

    primer commit

---
+++ bucle-foreach.js
@@ -0,0 +1,9 @@
+const personas=['Killian','Dani','Nika'];
+personas.forEach(function(persona,index){
+  // console.log(index);
+  console.log(persona);
+});
+
+personas.forEach((persona,index)=>{
+  console.log(persona) ;
+});
